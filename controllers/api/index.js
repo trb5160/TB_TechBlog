1af8dd79c23db475896c94f137885ea946542771
Tom Barber <trb5160@gmail.com>
2021-07-10 21:53:08 -0400

    fixed more pathing errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comments-route');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
