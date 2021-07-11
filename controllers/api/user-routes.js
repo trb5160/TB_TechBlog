a24e45dcf158bf97e67e675b81739abc7524ad7c
Tom Barber <trb5160@gmail.com>
2021-07-10 21:42:36 -0400

    accidentally put wrong name of file.  fixed

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-const withAuth = require('../../utils/authorization');
+const withAuth = require('../../utils/authentication');
 
 // GET /api/users
 router.get('/', (req, res) => {
